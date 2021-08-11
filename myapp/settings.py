f806a3d3710d4a03180b6ce185a146badcdfd883
homma <hommariku@hommanoMacBook-Pro.local>
2021-08-12 01:30:53 +0900

    コメントの追加

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todolist'
+    'todolist',# modelをプロジェクト内で有効にさせるためのコード
 ]
 
 MIDDLEWARE = [
